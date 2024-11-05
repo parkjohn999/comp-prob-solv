2a36e27ef5ac8e372df5838eaffdc49adc5a478e
parkjohn999 <parkjohn623@gmail.com>
2024-11-05 08:44:22 -0800

    Attempt Move Function

--- Project 1/GCMC.py
+++ Project 1/GCMC.py
@@ -33,3 +33,72 @@ def calculate_interaction_energy(lattice, site, particle, neighbor_indices, epsi
             else:
                 interaction_energy += epsilon_AB
     return interaction_energy
+
+def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indicies, params):
+    size = len(lattice)
+    N_sites = size * size
+    beta = 1/params['T']
+    epsilon_A, epsilon_B, epsilon_AA, epsilon_BB, epsilon_AB, mu_A, mu_B = params
+
+    np.random.seed(42)
+    if np.random.rand() > 0.5:
+        # Add a particle
+        if N_empty == 0:
+            return (N_A, N_B, N_empty)
+        # Random Empty Site in lattice
+        x = np.random.randint(size - 1)
+        y = np.random.randint(size - 1)
+        while lattice[x,y] != 0:
+            x = np.random.randint(size - 1)
+            y = np.random.randint(size - 1)
+        # Add Particle B
+        particle = 2
+        mu = mu_B
+        epsilon = epsilon_B
+        N_s = N_B
+        if np.random.rand() > 0.5:
+            # Add Particle A
+            particle = 1
+            mu = mu_A
+            epsilon = epsilon_A
+            N_s = N_A
+        delta_E = epsilon + calculate_interaction_energy(lattice,(x,y),particle,neighbor_indicies,epsilon_AA,epsilon_BB,epsilon_AB)
+        acc_prob = min[1, (N_empty) / (N_s + 1) * np.exp(-beta * (delta_E - mu))]
+        r = np.random.rand()
+        if r < acc_prob:
+            lattice[(x,y)] = particle
+            if particle == 1:
+                N_A += 1
+            else:
+                N_B += 1
+            N_empty -= 1
+    else:
+        # Remove a particle
+        if N_sites - N_empty == 0:
+            return (N_A, N_B, N_empty)
+        # Random Occupied Site in lattice
+        x = np.random.randint(size - 1)
+        y = np.random.randint(size - 1)
+        while not (lattice[x,y] == 0):
+            x = np.random.randint(size - 1)
+            y = np.random.randint(size - 1)
+        particle = lattice[(x,y)]
+        # Particle B
+        mu = mu_B
+        epsilon = epsilon_B
+        N_s = N_B
+        if particle == 1:
+            mu = mu_A
+            epsilon = epsilon_A
+            N_s = N_A
+        delta_E = -epsilon - calculate_interaction_energy(lattice,(x,y),particle,neighbor_indicies,epsilon_AA,epsilon_BB,epsilon_AB)
+        acc_prob = min[1, N_s / (N_empty + 1) * np.exp(-beta * (delta_E + mu))]
+        r = np.random.rand()
+        if r < acc_prob:
+            lattice[(x,y)] = 0
+            if particle == 1:
+                N_A -= 1
+            else:
+                N_B -= 1
+            N_empty += 1
+    return (N_A, N_B, N_empty)
